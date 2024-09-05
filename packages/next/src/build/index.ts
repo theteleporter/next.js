0dbfb304082a2b889f1306a51793eb1780471826
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-05 16:21:15 +0200

    print turbopack time

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1620,11 +1620,13 @@ export default async function build(
       let shutdownPromise = Promise.resolve()
       if (!isGenerateMode) {
         if (turboNextBuild) {
+          console.time('Turbopack build')
           const {
             duration: compilerDuration,
             shutdownPromise: p,
             ...rest
           } = await turbopackBuild()
+          console.timeEnd('Turbopack build')
           shutdownPromise = p
           traceMemoryUsage('Finished build', nextBuildSpan)
 
@@ -1649,6 +1651,7 @@ export default async function build(
               buildStage: 'compile-server',
             })
 
+            console.time('webpack build')
             const serverBuildPromise = webpackBuild(useBuildWorker, [
               'server',
             ]).then((res) => {
@@ -1716,6 +1719,7 @@ export default async function build(
               durationInSeconds += res.duration
               traceMemoryUsage('Finished client compilation', nextBuildSpan)
             })
+            console.timeEnd('webpack build')
 
             Log.event('Compiled successfully')
 
